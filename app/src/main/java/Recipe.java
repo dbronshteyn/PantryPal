b3ac3e9f1fd09155499d45ca2d283c0b6274169d
dbronshteynCS <daniel.bronshteyn81@gmail.com>
2023-11-02 17:22:57 -0700

    Fixed some things with the JSON file

---
+++ app/src/main/java/Recipe.java
@@ -0,0 +1,39 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.geometry.Insets;
+import javafx.scene.text.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Recipe extends HBox {
+    private Text title;
+
+    Recipe(String title) {
+        // this.setPrefSize(800, 20);
+        // this.setStyle("-fx-background-color: #DAE5EA; -fx-border-width: 0;
+        // -fx-font-weight: bold;");
+        this.title = new Text(title);
+        this.getChildren().add(this.title);
+    }
+}
