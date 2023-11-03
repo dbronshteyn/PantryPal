80c1eb34740708eb26d4d060cab7b64a507dac28
dbronshteynCS <daniel.bronshteyn81@gmail.com>
2023-11-03 10:33:07 -0700

    Finished the ChatGPT class and created the OpenAIManager class. workspace.xml is also updated.

--- app/src/main/java/backend/ChatGPT.java
+++ app/src/main/java/backend/ChatGPT.java
@@ -2,7 +2,6 @@ package backend;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -16,15 +15,17 @@ public class ChatGPT {
     private static final String API_KEY = "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";
     private static final String MODEL = "text-davinci-003";
 
-    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+    /*
+     * This code was just so that we can create a ChatGPT object in the future if
+     * needed. It is not necessary for the code to work. More to scale if needed.
+     */
+    boolean isInitialized = false;
 
-        // Refactor so I can pass in this for prompt and maxTokens:
-        // java ChatGPT 100 “What is the smallest country in the world?”
+    public ChatGPT() {
+        this.isInitialized = true;
+    }
 
-        // Set request parameters
-
-        String prompt = args[1];
-        int maxTokens = Integer.parseInt(args[0]);
+    public String generateText(String prompt, int maxTokens) {
 
         // Create a request body which you will pass into request object
         JSONObject requestBody = new JSONObject();
@@ -33,39 +34,33 @@ public class ChatGPT {
         requestBody.put("max_tokens", maxTokens);
         requestBody.put("temperature", 1.0);
 
-        // Create the HTTP Client
-
         HttpClient client = HttpClient.newHttpClient();
+        String responseBody = "";
 
         // Create the request object
-        HttpRequest request = HttpRequest
-                .newBuilder()
-                .uri(URI.create(API_ENDPOINT))
-                .header("Content-Type", "application/json")
-                .header("Authorization", String.format("Bearer %s", API_KEY))
-                .POST(HttpRequest.BodyPublishers.ofString(requestBody.toString()))
-                .build();
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(API_ENDPOINT))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer " + API_KEY)
+                    .POST(HttpRequest.BodyPublishers.ofString(requestBody.toString()))
+                    .build();
 
-        // Send the request and receive the response
+            HttpResponse<String> response = client.send(
+                    request,
+                    HttpResponse.BodyHandlers.ofString());
 
-        // Send the request and receive the response
+            // Process the response
+            responseBody = response.body();
 
-        HttpResponse<String> response = client.send(
-                request,
-                HttpResponse.BodyHandlers.ofString());
-
-        // Process the response
-        String responseBody = response.body();
-
-        System.out.println(responseBody);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return null; // or handle error appropriately
+        }
 
         JSONObject responseJson = new JSONObject(responseBody);
-
         JSONArray choices = responseJson.getJSONArray("choices");
-        String generatedText = choices.getJSONObject(0).getString("text");
-
-        System.out.println(generatedText);
-
+        return choices.getJSONObject(0).getString("text");
     }
 
 }
