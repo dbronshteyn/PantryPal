0ba60dce00329789d213144af5911c9d63e27669
PeterEckmann1 <53533143+PeterEckmann1@users.noreply.github.com>
2023-11-03 21:49:13 -0700

    completed feature 1

--- app/src/main/java/backend/Main.java
+++ app/src/main/java/backend/Main.java
@@ -1,9 +1,14 @@
 package backend;
 
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import ui.SceneController;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Main extends Application {
 
@@ -12,10 +17,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        SceneController root = new SceneController(primaryStage);
+        SceneController root = new SceneController();
         primaryStage.setTitle("PantryPal");
         primaryStage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
+
+        List<Recipe> recipes = new ArrayList<>();
+        recipes.add(new Recipe("Pasta", "Boil water, add pasta, add sauce", new Date()));
+        recipes.add(new Recipe("Pizza", "Buy pizza, heat oven, put pizza in oven", new Date()));
+        root.displayRecipeList(recipes);
     }
 
     public static void main(String[] args) {
