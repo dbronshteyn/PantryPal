f77458da14b5ac064d533dc1e2a6ab3f8c45375f
PeterEckmann1 <53533143+PeterEckmann1@users.noreply.github.com>
2023-11-04 21:39:39 -0700

    fixed bug with audio recording on mac and cleaned up code

--- app/src/main/java/ui/RecipeCreationScene.java
+++ app/src/main/java/ui/RecipeCreationScene.java
@@ -53,30 +53,25 @@ class RecipeCreationScene extends VBox {
         ToggleButton recordButton;
         AudioFormat audioFormat;
         TargetDataLine targetDataLine;
-        Thread t;
 
         AudioRecorder(String audioFilePath, ToggleButton recordButton) {
             this.audioFilePath = audioFilePath;
             this.recordButton = recordButton;
+            this.audioFormat = new AudioFormat(
+                44100,
+                16,
+                1,
+                true,
+                false);
         }
 
         public void recordAudio() {
-            // implement this part
-            // make sure to save the resulting audio file to this.audioFilePath   
-            // i also added the recordButton to this class, so when the recording 
-            // starts you can do recordButton.setText("Stop Recording");
-            
-            audioFormat = getAudioFormat();
-
             this.recordButton.setText("Stop Recording");
-
             Thread t = new Thread(
                 new Runnable() {
                     @Override
                     public void run() {
-                        try {
-                            startRecording();
-                        } catch (Exception e) {}
+                        startRecording();
                     }
                 }
             );
@@ -88,56 +83,23 @@ class RecipeCreationScene extends VBox {
             this.recordButton.setText("Start Recording");
         }
 
-        private AudioFormat getAudioFormat() {
-            // the number of samples of audio per second.
-            // 44100 represents the typical sample rate for CD-quality audio.
-            float sampleRate = 44100;
-
-            // the number of bits in each sample of a sound that has been digitized.
-            int sampleSizeInBits = 16;
-
-            // the number of audio channels in this format (1 for mono, 2 for stereo).
-            int channels = 2;
-
-            // whether the data is signed or unsigned.
-            boolean signed = true;
-
-            // whether the audio data is stored in big-endian or little-endian order.
-            boolean bigEndian = false;
-
-            return new AudioFormat(
-                    sampleRate,
-                    sampleSizeInBits,
-                    channels,
-                    signed,
-                    bigEndian);
-        }
-
         private void startRecording() {
             try {
-                // the format of the TargetDataLine
                 DataLine.Info dataLineInfo = new DataLine.Info(
-                        TargetDataLine.class,
-                        audioFormat);
-                // the TargetDataLine used to capture audio data from the microphone
+                    TargetDataLine.class,
+                    audioFormat);
                 targetDataLine = (TargetDataLine) AudioSystem.getLine(dataLineInfo);
                 targetDataLine.open(audioFormat);
                 targetDataLine.start();
-                //recordingLabel.setVisible(true);
-
-                // the AudioInputStream that will be used to write the audio data to a file
                 AudioInputStream audioInputStream = new AudioInputStream(
                         targetDataLine);
-
-                // the file that will contain the audio data
                 File audioFile = new File(audioFilePath);
                 AudioSystem.write(
                         audioInputStream,
                         AudioFileFormat.Type.WAVE,
                         audioFile);
-                //recordingLabel.setVisible(false);
-            } catch (Exception ex) {
-                ex.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
 
