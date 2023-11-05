1ecb593dafa0f15e7de3acbe5e3afba376b45d6d
dn0804 <78126917+dn0804@users.noreply.github.com>
2023-11-05 15:16:25 -0800

    Added documentation to backend classes

--- app/src/main/java/backend/Controller.java
+++ app/src/main/java/backend/Controller.java
@@ -5,6 +5,14 @@ import java.io.IOException;
 
 import ui.SceneController;
 
+/**
+ *  The Controller class is responsible for managing the recipes of this application.
+ * 
+ *  It initializes and interacts with a RecipeCreator, RecipeList and SceneController to create and show recipes, as well as dealing with user interactions.
+ * 
+ *  The class relies on an API key to access the ChatGPT and Whisper services.
+ */
+
 public class Controller {
 
     private static final String API_KEY = "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";
--- app/src/main/java/backend/Recipe.java
+++ app/src/main/java/backend/Recipe.java
@@ -2,10 +2,23 @@ package backend;
 
 import java.util.Date;
 
+/**
+ * The Recipe class represents a recipe with a title, instructions, and the date it was created on.
+ * It includes methods that can access and modify these fields, and employs a 'toString' method to display the recipe's title.
+ */
+
 public class Recipe {
     private String title;
     private String instructions;
     private Date dateCreated;
+
+
+    /**
+     * Constructs a new Recipe with the provided title, instructions, and creation date.
+     * @param title             Title of the recipe.
+     * @param instructions      Instructions for preparing the recipe.
+     * @param dateCreated       Date when the recipe was generated.
+     */
 
     public Recipe(String title, String instructions, Date dateCreated) {
         this.title = title;
@@ -37,6 +50,11 @@ public class Recipe {
         this.dateCreated = dateCreated;
     }
 
+    /**
+     * Returns a string representation of the recipe, or its title.
+     * 
+     * @return Title of the recipe.
+     */
     @Override
     public String toString() {
         return this.title;
