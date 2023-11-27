30568e0e2b3e371f7a86e06e1f0a5e2cc7712982
PeterEckmann1 <53533143+PeterEckmann1@users.noreply.github.com>
2023-11-26 22:18:41 -0800

    fixed tests

--- app/src/test/java/backend/RecipeTest.java
+++ app/src/test/java/backend/RecipeTest.java
@@ -1,97 +1,98 @@
-// package backend;
+package backend;
 
-// import org.json.JSONObject;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-// import java.util.Date;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Date;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// /**
-//  * Unit testing for Recipe class
-//  */
-// class RecipeTest {
-//     private Recipe recipe;
-//     private Date dateCreated;
+/**
+ * Unit testing for Recipe class
+ */
+class RecipeTest {
+    private Recipe recipe;
+    private Date dateCreated;
 
-//     /**
-//      * Sets up the RecipeTest.
-//      */
-//     @BeforeEach
-//     public void setUp() {
-//         dateCreated = new Date(0);
-//         recipe = new Recipe("id 1", "Chocolate Cake", "Mix ingredients and bake for 30 minutes.", dateCreated);
-//     }
+    /**
+     * Sets up the RecipeTest.
+     */
+    @BeforeEach
+    public void setUp() {
+        dateCreated = new Date(0);
+        recipe = new Recipe("id 1", "Chocolate Cake", "Mix ingredients and bake for 30 minutes.", dateCreated, "");
+    }
 
-//     /**
-//      * Tests that the Recipe properly sets the instructions.
-//      */
-//     @Test
-//     void testSetInstructions() {
-//         String newInstructions = "Mix ingredients and bake for 45 minutes.";
-//         Date oldDate = recipe.getDateCreated();
-//         recipe.setInstructions(newInstructions);
-//         assertEquals(newInstructions, recipe.getInstructions());
-//         assertTrue(recipe.getDateCreated().after(oldDate));
-//     }
+    /**
+     * Tests that the Recipe properly sets the instructions.
+     */
+    @Test
+    void testSetInstructions() {
+        String newInstructions = "Mix ingredients and bake for 45 minutes.";
+        Date oldDate = recipe.getDateCreated();
+        recipe.setInstructions(newInstructions);
+        assertEquals(newInstructions, recipe.getInstructions());
+        assertTrue(recipe.getDateCreated().after(oldDate));
+    }
 
-//     /**
-//      * Tests that the Recipe properly returns the title.
-//      */
-//     @Test
-//     void testToString() {
-//         assertEquals("Chocolate Cake", recipe.toString());
-//     }
+    /**
+     * Tests that the Recipe properly returns the title.
+     */
+    @Test
+    void testToString() {
+        assertEquals("Chocolate Cake", recipe.toString());
+    }
 
-//     /**
-//      * Tests Recipe properly reads JSON Object.
-//      */
-//     @Test
-//     void testFromJSON() {
-//         JSONObject json = new JSONObject(
-//                 "{\"title\":\"abc\",\"instructions\":\"ab\",\"dateCreated\":\"1970-01-01T00:00:00-00:00\",\"recipeID\":\"id 1\"}");
-//         Recipe recipe = new Recipe(json);
-//         assertEquals("id 1", recipe.getRecipeID());
-//         assertEquals("abc", recipe.getTitle());
-//         assertEquals("ab", recipe.getInstructions());
-//         assertEquals(new Date(0), recipe.getDateCreated());
-//     }
+    /**
+     * Tests Recipe properly reads JSON Object.
+     */
+    @Test
+    void testFromJSON() {
+        JSONObject json = new JSONObject(
+                "{\"title\":\"abc\",\"instructions\":\"ab\",\"dateCreated\":\"1970-01-01T00:00:00-00:00\",\"recipeID\":\"id 1\",\"accountUsername\":\"username 1\"}");
+        Recipe recipe = new Recipe(json);
+        assertEquals("id 1", recipe.getRecipeID());
+        assertEquals("abc", recipe.getTitle());
+        assertEquals("ab", recipe.getInstructions());
+        assertEquals(new Date(0), recipe.getDateCreated());
+        assertEquals("username 1", recipe.getAccountUsername());
+    }
 
-//     /**
-//      * Tests to JSON file.
-//      */
-//     @Test
-//     void testToJSON() {
-//         JSONObject json = recipe.toJSON();
-//         assertEquals("Chocolate Cake", json.getString("title"));
-//         assertEquals("Mix ingredients and bake for 30 minutes.", json.getString("instructions"));
+    /**
+     * Tests to JSON file.
+     */
+    @Test
+    void testToJSON() {
+        JSONObject json = recipe.toJSON();
+        assertEquals("Chocolate Cake", json.getString("title"));
+        assertEquals("Mix ingredients and bake for 30 minutes.", json.getString("instructions"));
 
-//         // this one seems to depend on the system
-//         assertTrue(
-//                 json.getString("dateCreated").startsWith("1969") || json.getString("dateCreated").startsWith("1970"));
-//     }
+        // this one seems to depend on the system
+        assertTrue(
+                json.getString("dateCreated").startsWith("1969") || json.getString("dateCreated").startsWith("1970"));
+    }
 
-//     /*
-//      * Tests editing a recipe
-//      */
-//     @Test
-//     void testEditRecipe() {
-//         long currentTime = System.currentTimeMillis();
-//         Recipe recipe = new Recipe("", "Muffins", "Add 1 cup of sugar and flour.", new Date(currentTime - 1000));
-//         recipe.setInstructions("Add 1/2 cup of sugar and flour.");
-//         assertEquals("Add 1/2 cup of sugar and flour.", recipe.getInstructions());
-//         assertTrue(recipe.getDateCreated().after(new Date(currentTime - 1000)));
-//     }
+    /*
+     * Tests editing a recipe
+     */
+    @Test
+    void testEditRecipe() {
+        long currentTime = System.currentTimeMillis();
+        Recipe recipe = new Recipe("", "Muffins", "Add 1 cup of sugar and flour.", new Date(currentTime - 1000), "");
+        recipe.setInstructions("Add 1/2 cup of sugar and flour.");
+        assertEquals("Add 1/2 cup of sugar and flour.", recipe.getInstructions());
+        assertTrue(recipe.getDateCreated().after(new Date(currentTime - 1000)));
+    }
 
-//     /**
-//      * Tests retrieving recipe details
-//      */
-//     @Test
-//     void testGetRecipeDetails() {
-//         assertEquals("Chocolate Cake", recipe.getTitle());
-//         assertEquals("Mix ingredients and bake for 30 minutes.", recipe.getInstructions());
-//         assertEquals("id 1", recipe.getRecipeID());
-//         assertEquals(dateCreated, recipe.getDateCreated());
-//     }
-// }
+    /**
+     * Tests retrieving recipe details
+     */
+    @Test
+    void testGetRecipeDetails() {
+        assertEquals("Chocolate Cake", recipe.getTitle());
+        assertEquals("Mix ingredients and bake for 30 minutes.", recipe.getInstructions());
+        assertEquals("id 1", recipe.getRecipeID());
+        assertEquals(dateCreated, recipe.getDateCreated());
+    }
+}
