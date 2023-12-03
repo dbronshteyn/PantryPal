6dac9e58195ffbb476fe4000f76c7715d21bf8e2
dbronshteynCS <daniel.bronshteyn81@gmail.com>
2023-12-02 16:07:42 -0800

    Add doc to account list and DallE classes

--- app/src/main/java/backend/AccountList.java
+++ app/src/main/java/backend/AccountList.java
@@ -1,37 +1,60 @@
 package backend;
 
 import java.util.List;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import java.io.File;
 
+/**
+ * This class represents a list of accounts that can be used to login to the
+ * application.
+ */
 public class AccountList {
 
+    /**
+     * The file where the accounts are stored.
+     */
     private File databaseFile;
     private List<Account> accounts;
 
+    /**
+     * This class represents an account with a username and password.
+     */
     public class Account {
         private String username;
         private String password;
 
+        /**
+         * Constructs an account with the specified username and password.
+         * 
+         * @param username the username of the account
+         * @param password the password of the account
+         */
         public Account(String username, String password) {
             this.username = username;
             this.password = password;
         }
 
+        /**
+         * Constructs an account from a JSON object.
+         * 
+         * @param jsonAccount the JSON object representing the account
+         */
         public Account(JSONObject jsonAccount) {
             this.username = jsonAccount.getString("username");
             this.password = jsonAccount.getString("password");
         }
 
+        /**
+         * Returns the JSON object representing the account.
+         * 
+         * @return the JSON object representing the account
+         */
         public JSONObject toJSON() {
             JSONObject jsonAccount = new JSONObject();
             jsonAccount.put("username", this.username);
@@ -39,25 +62,58 @@ public class AccountList {
             return jsonAccount;
         }
 
+        /**
+         * Returns the username of the account.
+         * 
+         * @return the username of the account
+         */
         public String getUsername() {
             return this.username;
         }
 
+        /**
+         * Returns the password of the account.
+         * 
+         * @return the password of the account
+         */
         public String getPassword() {
             return this.password;
         }
 
+        // When I click save for the file it formats the comment like this, can't fix
+        /**
+         * Returns true if the specified username and password match the username and
+         * password of the account.
+         * 
+         * @param username the username to check
+         * @param password the password to check
+         * @return true if the specified username and password match the username and
+         *         password of the account
+         */
         public boolean matchesCredentials(String username, String password) {
             return this.username.equals(username) && this.password.equals(password);
         }
     }
 
+    /**
+     * Constructs an account list with the specified database file.
+     * 
+     * @param databaseFile the file where the accounts are stored
+     */
     public AccountList(File databaseFile) {
         this.databaseFile = databaseFile;
         this.accounts = new ArrayList<>();
         this.loadAccountsFromFile();
     }
 
+    /**
+     * Adds an account with the specified username and password to the account list.
+     * 
+     * @param username the username of the account to add
+     * @param password the password of the account to add
+     * @return true if the account was added, false if an account with the specified
+     *         username already exists
+     */
     public boolean addAccount(String username, String password) {
         for (Account account : this.accounts) {
             if (account.getUsername().equals(username)) {
@@ -69,6 +125,13 @@ public class AccountList {
         return true;
     }
 
+    /**
+     * Attempts to login with the specified username and password.
+     * 
+     * @param username the username to login with
+     * @param password the password to login with
+     * @return true if the login was successful, false otherwise
+     */
     public boolean attemptLogin(String username, String password) {
         for (Account account : this.accounts) {
             if (account.matchesCredentials(username, password)) {
@@ -78,6 +141,12 @@ public class AccountList {
         return false;
     }
 
+    /**
+     * Attempts to logout the specified user.
+     * 
+     * @param username the username to logout
+     * @return true if the logout was successful, false otherwise
+     */
     public boolean attemptLogout(String username) {
         File automaticLoginFile = new File("automaticLogin.json");
         if (automaticLoginFile.exists()) {
@@ -87,10 +156,18 @@ public class AccountList {
         return false;
     }
 
+    /**
+     * Returns the list of accounts.
+     * 
+     * @return the list of accounts
+     */
     public List<Account> getAccounts() {
         return this.accounts;
     }
 
+    /**
+     * Loads the accounts from the database file.
+     */
     public void loadAccountsFromFile() {
         if (this.databaseFile.exists()) {
             try {
@@ -107,6 +184,9 @@ public class AccountList {
         }
     }
 
+    /**
+     * Updates the database file with the current list of accounts.
+     */
     public void updateDatabase() {
         JSONArray jsonAccountList = new JSONArray();
         for (Account account : this.accounts) {
@@ -122,8 +202,12 @@ public class AccountList {
         }
     }
 
+    /**
+     * Returns the list of accounts.
+     * 
+     * @return the list of accounts
+     */
     public List<Account> getList() {
         return accounts;
     }
-
 }
