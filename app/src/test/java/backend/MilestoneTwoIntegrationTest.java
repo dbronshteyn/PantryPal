e3ffa2413fcca05e0ae57025cbc18790841dbcef
PeterEckmann1 <53533143+PeterEckmann1@users.noreply.github.com>
2023-12-04 22:47:56 -0800

    added integration test iteration 2 test 1

--- app/src/test/java/backend/MilestoneTwoIntegrationTest.java
+++ app/src/test/java/backend/MilestoneTwoIntegrationTest.java
@@ -73,4 +73,52 @@ class MilestoneTwoIntegrationTest {
         assertEquals("hex of eggs and bacon", recipe.getImageHex());
         recipeList.addRecipe(recipe);
     }
+
+    @Test
+    void testCaitlinEnjoysTheNewFeaturesOfPantryPalTwo() throws IOException, InterruptedException, URISyntaxException {
+        assertTrue(accountList.addAccount("Caitlin", "password123"));
+        assertTrue(accountList.attemptLogin("Caitlin", "password123"));
+
+        RecipeBuilder builder = new RecipeBuilder(chatGPTMock, whisperMock, dallEMock);
+        whisperMock.setMockScenario("breakfast.wav", "breakfast");
+        builder.getMealTypeElement().specify(new File("breakfast.wav"));
+        whisperMock.setMockScenario("eggs-and-cheese.wav", "Eggs and cheese");
+        builder.getIngredientsElement().specify(new File("eggs-and-cheese.wav"));
+        chatGPTMock.setMockScenario("Please provide a recipe with a title denoted with \"Title:\", a new line, and then a detailed recipe. Create a breakfast recipe with the following ingredients: Eggs and cheese", "Title: Eggs and cheese\nCook");
+        dallEMock.setMockScenario("Eggs and cheese", "hex of eggs and cheese");
+        Recipe recipe = builder.returnRecipe("Caitlin");
+        assertEquals("hex of eggs and cheese", recipe.getImageHex());
+        recipeList.addRecipe(recipe);
+        
+        builder = new RecipeBuilder(chatGPTMock, whisperMock, dallEMock);
+        whisperMock.setMockScenario("dinner.wav", "dinner");
+        builder.getMealTypeElement().specify(new File("dinner.wav"));
+        whisperMock.setMockScenario("pasta-and-tomato-sauce.wav", "Pasta and tomato sauce");
+        builder.getIngredientsElement().specify(new File("pasta-and-tomato-sauce.wav"));
+        chatGPTMock.setMockScenario("Please provide a recipe with a title denoted with \"Title:\", a new line, and then a detailed recipe. Create a dinner recipe with the following ingredients: Pasta and tomato sauce", "Title: Pasta Marinara\nCook");
+        dallEMock.setMockScenario("Pasta Marinara", "hex of pasta marinara");
+        recipe = builder.returnRecipe("Caitlin");
+        assertEquals("hex of pasta marinara", recipe.getImageHex());
+        recipeList.addRecipe(recipe);
+
+        assertEquals("dinner", recipeList.getRecipeByID(recipeList.getRecipeIDs("Caitlin", "most-recent", "all").get(0)).getMealType());
+        assertEquals("breakfast", recipeList.getRecipeByID(recipeList.getRecipeIDs("Caitlin", "most-recent", "all").get(1)).getMealType());
+
+        assertEquals("Pasta Marinara", recipeList.getRecipeByID(recipeList.getRecipeIDs("Caitlin", "most-recent", "all").get(0)).getTitle());
+
+        assertEquals("Eggs and cheese", recipeList.getRecipeByID(recipeList.getRecipeIDs("Caitlin", "a-z", "all").get(0)).getTitle());
+
+        assertEquals("Pasta Marinara", recipeList.getRecipeByID(recipeList.getRecipeIDs("Caitlin", "z-a", "all").get(0)).getTitle());
+
+        assertEquals("Eggs and cheese", recipeList.getRecipeByID(recipeList.getRecipeIDs("Caitlin", "least-recent", "all").get(0)).getTitle());
+
+        assertEquals(2, recipeList.getRecipeIDs("Caitlin", "most-recent", "all").size());
+
+        assertEquals(1, recipeList.getRecipeIDs("Caitlin", "most-recent", "breakfast").size());
+        assertEquals("Eggs and cheese", recipeList.getRecipeByID(recipeList.getRecipeIDs("Caitlin", "most-recent", "breakfast").get(0)).getTitle());
+
+        assertEquals(2, recipeList.getRecipeIDs("Caitlin", "most-recent", "all").size());
+
+        assertEquals("<html><body style=\"background-color: #e7ffe6; font-family: Arial;\"><h1>Eggs and cheese</h1><img src=\"data:image/png;base64,/u///u/vn8+/7v4=\" alt=\"Recipe Image\"><p>Cook</p></body></html>", recipeList.getRecipeByID(recipeList.getRecipeIDs("Caitlin", "a-z", "all").get(0)).toHTML());
+    }
 }
